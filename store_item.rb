e63b3729dcd1e70ca8bf1dc892e70dd224dba062
Paolo Maloles <jpaolomaloles@gmail.com>
2023-05-02 15:44:07 -0500

    Added the class version, added text for hash version

--- store_item.rb
+++ store_item.rb
@@ -9,6 +9,57 @@ balloonstore = [
   { :color => "green", :price => "3.99", :texture => "smooth" },
 ]
 
+puts "------------------------------"
+puts "this is the Hash version"
+puts
 puts "The #{balloonstore[0][:texture]}, #{balloonstore[0][:color]} balloon costs #{balloonstore[0][:price]}"
 puts "The #{balloonstore[1][:texture]}, #{balloonstore[1][:color]} balloon costs #{balloonstore[1][:price]}"
 puts "The #{balloonstore[2][:texture]}, #{balloonstore[2][:color]} balloon costs #{balloonstore[2][:price]}"
+
+# Exercise: Rewrite your store items using a class instead of a hash.
+# a) Choose which attributes should have “reader” methods and which attributes should have “writer” methods.
+# b) Create an instance from your store item class. Use puts statements to print the 3 attributes individually to the terminal.
+
+class Balloon
+  def initialize(color, price, texture)
+    @color = color
+    @price = price
+    @texture = texture
+  end
+
+  def color
+    @color
+  end
+
+  def color=(color)
+    @color = color
+  end
+
+  def price
+    @price
+  end
+
+  def price=(price)
+    @price = price
+  end
+
+  def texture
+    @texture
+  end
+
+  def texture=(texture)
+    @texture = texture
+  end
+end
+
+balloon1 = Balloon.new("red", 1.99, "bumpy")
+balloon2 = Balloon.new("blue", 2.99, "sticky")
+balloon3 = Balloon.new("green", 3.99, "smooth")
+
+puts
+puts "------------------------------"
+puts "this is the Class version"
+puts
+puts "The #{balloon1.texture}, #{balloon1.color} balloon costs #{balloon1.price}"
+puts "The #{balloon2.texture}, #{balloon2.color} balloon costs #{balloon2.price}"
+puts "The #{balloon3.texture}, #{balloon3.color} balloon costs #{balloon3.price}"
