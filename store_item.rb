f7f9f813c03c70e520ba86d4c6f829f329834b94
Paolo Maloles <jpaolomaloles@gmail.com>
2023-05-02 14:10:36 -0500

    Made a balloon store

--- store_item.rb
+++ store_item.rb
@@ -1,7 +1,10 @@
 #C.1 - You are the owner of a store that sells items (you decide what specifically). Each item has various properties such as color, price, etc.
-balloonstore =
 
 #C.2 - Represent 3 items using hashes. Each hash should have the same keys with different values.
 
-
 #C.3 - Be sure to use symbols for the keys. Try creating hashes using both types of hash symbol syntaxes. (Ruby syntax {:a => 123} vs. “JavaScript” syntax {a: 123}).
+balloonstore = [
+  { color: "Red", price: "1.99", texture: "bumpy" },
+  { color: "Blue", price: "2.99", texture: "sticky" },
+  { color: "Green", price: "3.99", texture: "smooth" },
+]
