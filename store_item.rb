8aee89f348b117dc1ec86a6bc5ec7197c0cabac8
Paolo Maloles <jpaolomaloles@gmail.com>
2023-05-02 14:16:54 -0500

    modified capitalization of colors, put out printing

--- store_item.rb
+++ store_item.rb
@@ -4,7 +4,11 @@
 
 #C.3 - Be sure to use symbols for the keys. Try creating hashes using both types of hash symbol syntaxes. (Ruby syntax {:a => 123} vs. “JavaScript” syntax {a: 123}).
 balloonstore = [
-  { color: "Red", price: "1.99", texture: "bumpy" },
-  { color: "Blue", price: "2.99", texture: "sticky" },
-  { :color => "Green", :price => "3.99", :texture => "smooth" },
+  { color: "red", price: "1.99", texture: "bumpy" },
+  { color: "blue", price: "2.99", texture: "sticky" },
+  { :color => "green", :price => "3.99", :texture => "smooth" },
 ]
+
+puts "The #{balloonstore[0][:texture]}, #{balloonstore[0][:color]} balloon costs #{balloonstore[0][:price]}"
+puts "The #{balloonstore[1][:texture]}, #{balloonstore[1][:color]} balloon costs #{balloonstore[1][:price]}"
+puts "The #{balloonstore[2][:texture]}, #{balloonstore[2][:color]} balloon costs #{balloonstore[2][:price]}"
