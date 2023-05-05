ac35f6dc718473eaac1937c5d559dc57e09bc3ed
Paolo Maloles <jpaolomaloles@gmail.com>
2023-05-05 13:15:59 -0500

    Made it print all definitions, all examples, all pronunciations

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -9,30 +9,58 @@ word = gets.chomp
 definition = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=#{YOURAPIKEY}")
 
 definition_data = definition.parse(:json)
-#List of Definitions
 
-# definition_list = []
-# definition_data.each do |adefinition|
-#   adefinition.each do |specificdefinition|
-#     specificdefinition << adefinition["text"]
-#   end
-# end
-definitiontext = definition_data[1]["text"]
+definition_list = []
+definition_data.each do |wordinstance| # 1
+  if wordinstance["text"] != nil
+    definition_list << wordinstance["text"]
+  end
+end
+
+#definitiontext = definition_data[1]["text"]
 puts
-puts "Definition: #{definitiontext}"
+#puts "Definition: #{definitiontext}"
+puts "Definitions:"
+definition_list.each do |definition|
+  puts "----#{definition}"
+end
 puts
 puts "-----------------------------------------------------------------------------------------------------------------------------------"
 examples = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/examples?includeDuplicates=false&useCanonical=false&limit=5&api_key=#{YOURAPIKEY}")
 
 examples_data = examples.parse(:json)
-topexample = examples_data["examples"][0]["text"]
-puts "Here is an example of #{word} being used in a sentence:"
+multiexample = examples_data["examples"]
 
-p topexample
+examples_list = []
+multiexample.each do |wordinstance| # 1
+  if wordinstance["text"] != nil
+    examples_list << wordinstance["text"]
+  end
+end
+
+#topexample = examples_data["examples"][0]["text"]
+
+puts "Here is an example of '#{word}' being used in a sentence:"
+
+#p topexample
+examples_list.each do |example|
+  puts "----#{example}"
+end
 puts
 puts "-----------------------------------------------------------------------------------------------------------------------------------"
 pronunciation = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/pronunciations?useCanonical=false&limit=50&api_key=#{YOURAPIKEY}")
 pronunciation_data = pronunciation.parse(:json)
-pronunciationtext = pronunciation_data[0]["raw"]
+#pronunciationtext = pronunciation_data[0]["raw"]
 
-puts "The pronunciation is: #{pronunciationtext}"
+pronunciations_list = []
+pronunciation_data.each do |wordinstance| # 1
+  if wordinstance["raw"] != nil
+    pronunciations_list << wordinstance["raw"]
+  end
+end
+
+#puts "The pronunciation is: #{pronunciationtext}"
+puts "Here are pronunciations of '#{word}':"
+pronunciations_list.each do |pronunciation|
+  puts "----#{pronunciation}"
+end
