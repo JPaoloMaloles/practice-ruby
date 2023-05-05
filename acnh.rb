0d16d8bc66bb759aba12b4a0d0052f03b1dfe017
Paolo Maloles <jpaolomaloles@gmail.com>
2023-05-05 16:00:57 -0500

    made animal crossing print out all names of languages (without repeat) even if different region

--- acnh.rb
+++ acnh.rb
@@ -5,12 +5,32 @@ def checknumber(idinput)
   return idinput.scan(/\D/).empty?
 end
 
-villagerid = "a"
-while !checknumber(villagerid)
+villager_id = "a"
+while !checknumber(villager_id)
   puts "Please put in the village ID (1-413):"
-  villagerid = gets.chomp
+  villager_id = gets.chomp
 end
 
-villager_example = HTTP.get("https://acnhapi.com/v1/villagers/#{villagerid}")
+villager_example = HTTP.get("https://acnhapi.com/v1/villagers/#{villager_id}")
 villager_data = villager_example.parse(:json)
-p villager_data
+pp villager_data
+puts
+
+regions = ["USen", "EUen", "EUde", "EUes", "USes", "EUfr", "USfr", "EUit", "EUnl", "CNzh", "TWzh", "JPja", "KRko", "EUru"]
+
+villager_name = villager_data["name"]["name-USen"]
+villager_personality = villager_data["personality"]
+villager_birthdaystring = villager_data["birthday-string"]
+villager_birthday = villager_data["birthday"]
+puts "ID: #{villager_id}"
+alreadydone = []
+regions.each do |acronym|
+  if !alreadydone.include?(acronym[2] + acronym[3])
+    puts "Name (#{acronym[2] + acronym[3]}): #{villager_data["name"]["name-#{acronym}"]}"
+    alreadydone << acronym[2] + acronym[3]
+    #puts "alreadydone is:"
+    #p alreadydone
+  end
+end
+puts "Personality: #{villager_personality}"
+puts "Birthday: #{villager_birthdaystring} (#{villager_birthday})"
